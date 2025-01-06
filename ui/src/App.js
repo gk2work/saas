7e4ed4533a42fd5c1fbf8b1ff44543960529a021
gautamkumar <gkt2work@gmail.com>
2025-01-06 23:30:24 +0530

    feat: react app setup

---
+++ ui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div></div>
+     <header className='header'>
+
+     </header>
+     <section></section>
+     <footer></footer>
+    </div>
+  );
+}
+
+export default App;
